7fc04e09baa7b3cc9fb3697fc3d11d630c5d9a1f
tbswang <gt2017qd@163.com>
2024-11-22 10:06:26 +0800

    版本号排序

--- sort-version.js
+++ sort-version.js
@@ -1,22 +1,44 @@
 // 示例用法
-var versions = ["1.45.0", "1.5", "6", "3.3.3.3.3.3.3"];
-var sortedVersions = sortVersion(versions);
-console.log(sortedVersions);
+// var sortedVersions = sortVersion(versions);
+// console.log(sortedVersions);
 
+// var versions = [];
+const versions = ["1.0.0", "1.2", "1.1.1", "1.1", "1.45.0", "1.5", "6", "3.3.3.3.3.3.3"];
 
 function sortVersion(versions) {
   return versions.sort((a, b) => {
-    const arrA = a.split('.')
-    const arrB = b.split('.')
+    const arrA = a.split('.').map(Number)
+    const arrB = b.split('.').map(Number)
     for (let i = 0; i < Math.max(arrA.length, arrB.length); i++) {
-      if (arrA[i] > arrB[i]) {
-        return true
-      } else if (arrA < arrB[i]) {
-        return false
-      } else {
-        return 0
+      const numA = arrA[i] || 0; // 默认值 0
+      const numB = arrB[i] || 0; // 默认值 0
+      if (numA > numB) {
+        return 1
+      } else if (numA < numB) {
+        return -1
       }
     }
+    return 0
   })
-
 }
+
+function sortVersion2(versionList) {
+  return versionList.sort((verA, verB) => {
+    const verAList = verA.split('.')
+    const verBList = verB.split('.')
+    while (verAList.length > 0 && verBList.length > 0) {
+      if (verAList[0] > verBList[0]) {
+        return 1
+      } else if (verAList[0] < verBList[0]) {
+        return -1
+      } else {
+        verAList.shift()
+        verBList.shift()
+      }
+    }
+    return 0
+  })
+}
+
+console.log(sortVersion(versions));
+// console.log(sortVersion2(versions));
